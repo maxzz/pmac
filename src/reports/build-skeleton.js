e690cca9c9eef29896cc1f698c4c86a595a7f2f1
Max Zakharzhevskiy <maxzz@msn.com>
2022-09-06 00:02:06 -0700

    WIP. reports. update build skeleton. 1

--- src/reports/build-skeleton.js
+++ src/reports/build-skeleton.js
@@ -1,1 +1,24 @@
-console.log('built');
+const fs = require('fs');
+const path = require('path');
+
+function main() {
+    const sriptDir = path.dirname(process.argv[1]);
+    const fname = path.resolve(sriptDir, './index--single.html');
+
+    const cnt = fs.readFileSync(fname);
+
+    const b64 = cnt.toString('base64');
+    console.log('b64', b64);
+
+    // const b64 = btoa(cnt.toString());
+    // console.log('b64', b64);
+
+    const a = atob(b64);
+    console.log('a', a);
+
+    // const cnt = fs.readFileSync(fname).toString();
+    // const b64 = Buffer.from(cnt, 'base64');
+    // console.log('b64', b64.toString());
+}
+
+main();
