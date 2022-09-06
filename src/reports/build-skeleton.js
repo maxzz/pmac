bfd0ee0563362c7012c4a7604fdb143640b62d79
Max Zakharzhevskiy <maxzz@msn.com>
2022-09-06 00:14:55 -0700

    WIP. reports. update build skeleton. 2

--- src/reports/build-skeleton.js
+++ src/reports/build-skeleton.js
@@ -2,23 +2,20 @@ const fs = require('fs');
 const path = require('path');
 
 function main() {
-    const sriptDir = path.dirname(process.argv[1]);
-    const fname = path.resolve(sriptDir, './index--single.html');
+    const rootDir = path.dirname(process.argv[1]); // get this script folder
 
-    const cnt = fs.readFileSync(fname);
+    const fnameSk = path.resolve(rootDir, './index--single.html');
+    const cnt = fs.readFileSync(fnameSk);
 
     const b64 = cnt.toString('base64');
-    console.log('b64', b64);
+    //console.log('b64', b64);
 
-    // const b64 = btoa(cnt.toString());
-    // console.log('b64', b64);
+    const newCnt = `{ "skeleton": "${b64}" }`
+    const fnameDt = path.resolve(rootDir, './packed64-html.json');
+    fs.writeFileSync(fnameDt, newCnt);
 
-    const a = atob(b64);
-    console.log('a', a);
-
-    // const cnt = fs.readFileSync(fname).toString();
-    // const b64 = Buffer.from(cnt, 'base64');
-    // console.log('b64', b64.toString());
+    // const a = atob(b64);
+    // console.log('a', a);
 }
 
 main();
