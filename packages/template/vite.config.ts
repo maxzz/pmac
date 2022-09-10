ab42303092d2bd83db11d89e44c04c0fac2e32e9
Max Zakharzhevskiy <maxzz@msn.com>
2022-09-09 18:18:58 -0700

    WIP. @pmac/template. remove vitejs polyfillModulePreload

--- packages/template/vite.config.ts
+++ packages/template/vite.config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
     base: '',
+    build: {
+        polyfillModulePreload: false,
+    },
     server: {
         port: 3000,
     }
