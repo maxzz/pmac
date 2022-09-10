4e5d7f06a1f11c81cb0c50348fa0030ffd682ec6
Max Zakharzhevskiy <maxzz@msn.com>
2022-09-09 19:26:34 -0700

    WIP. @pmac/template. initial tailwindcss setup

--- packages/template/src/main.ts
+++ packages/template/src/main.ts
@@ -1,29 +1,36 @@
 import '../index.css';
-//import './style.css'
-//import typescriptLogo from './typescript.svg'
-//import { setupCounter } from './counter'
+import { setupCounter } from './counter';
 
 const typescriptLogo = '';
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div class="bg-red-200">
+const app = document.querySelector<HTMLDivElement>('#app')!;
+
+app.innerHTML = 
+`
+  <div class="flex-1 bg-gradient-to-t from-blue-500 to-blue-400">
+
     <a href="https://vitejs.dev" target="_blank">
       <!-- <img src="/vite.svg" class="logo" alt="Vite logo" /> -->
     </a>
+
     <a href="https://www.typescriptlang.org/" target="_blank">
       <!-- <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" /> -->
     </a>
+
     <h1>Vite + TypeScript</h1>
+
     <div class="card">
-      <button id="counter" type="button"></button>
+      <button class="px-2 p-1 border-primary-700 border rounded" id="counter" type="button"></button>
     </div>
+
     <p class="read-the-docs">
       Click on the Vite and TypeScript logos to learn more
     </p>
+
   </div>
 `;
 
-//setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+setupCounter(document.querySelector<HTMLButtonElement>('#counter')!);
 
 const reportData = (window as any)['reportData'] as string;
 
