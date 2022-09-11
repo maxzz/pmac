b128c75e75ba7091cbd5d1faff62b037d0c0cddb
Max Zakharzhevskiy <maxzz@msn.com>
2022-09-11 00:53:55 -0700

    WIP. @pmac/template. update app layout

--- packages/template/src/components/components.ts
+++ packages/template/src/components/components.ts
@@ -4,9 +4,12 @@ export function H1(text: string) {
     `);
 }
 
-export function Button(id: string, text?: string) {
+export function Button({ id, text }: { id: string, text?: string; }) {
     return (`
-    <button class="px-3 pt-1 pb-2 bg-primary-300/30 border-primary-700 hover:bg-primary-300/70 shadow active:scale-[.97] border rounded transition-all" id="${id}" type="button">${text || ''}</button>
+    <button class="px-3 pt-1 pb-2 
+        bg-primary-300/30 border-primary-700 hover:bg-primary-300/70 shadow active:scale-[.97] border rounded transition-all" 
+        id="${id}" type="button">${text || ''}
+    </button>
     `);
 }
 
--- packages/template/src/main.ts
+++ packages/template/src/main.ts
@@ -3,24 +3,29 @@ import { report } from './utils/test-data';
 import '../index.css';
 import { TableAllInputs } from './components/tables';
 
+function App() {
+    return (`
+        <div class="flex-1 bg-gradient-to-t from-primary-50 to-[#036eb3] text-primary-900">
+            ${PageHeader()}
+
+            <div class="px-4 p-2 grid grid-cols-[1fr_minmax(100px,auto)_1fr]">
+                <div class="col-start-2">
+                    ${H1("All files")}
+
+                    ${report.inputs ? TableAllInputs(report.inputs) : ''}
+
+                    <br />
+                    ${Button({ id: "counter" })}
+
+                    <!-- ${Para("Click on the Vite and TypeScript logos to learn more")} -->
+                </div>
+            </div>
+        </div>
+    `);
+}
+
 const app = document.querySelector<HTMLDivElement>('#app')!;
-app.innerHTML =
-    `
-  <div class="flex-1 bg-gradient-to-t from-primary-50 to-[#036eb3] text-primary-900">
-    ${PageHeader()}
-
-    <div class="px-4 p-2">
-        ${H1("All files")}
-
-        ${report.inputs ? TableAllInputs(report.inputs) : ''}
-
-        <br />
-        ${Button("counter")}
-
-        <!-- ${Para("Click on the Vite and TypeScript logos to learn more")} -->
-    </div>
-  </div>
-`;
+app.innerHTML = App();
 
 setupCounter(document.querySelector<HTMLButtonElement>('#counter')!);
 
