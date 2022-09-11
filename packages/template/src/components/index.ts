671152c08c031125e772935cedb8b2c56da1d754
Max Zakharzhevskiy <maxzz@msn.com>
2022-09-10 18:52:05 -0700

    WIP. @pmac/template. reorganize and add components folder

---
+++ packages/template/src/components/index.ts
@@ -0,0 +1,3 @@
+export { Button, H1, Para } from './components';
+export { setupCounter } from './counter';
+export { PageHeader } from './header';
--- packages/template/src/main.ts
+++ packages/template/src/main.ts
@@ -1,26 +1,6 @@
+import { Button, H1, PageHeader, Para, setupCounter } from './components';
+import { report } from './utils/test-data';
 import '../index.css';
-import { setupCounter } from './counter';
-import { PageHeader } from './header';
-//import { Report, Report_InputFiles } from '@pmac/shared-types';
-import { report } from './test-data';
-
-function H1(text: string) {
-    return (`
-    <h1 class="text-lg font-semibold" title="List of all files">${text}</h1>
-    `);
-}
-
-function Button(id: string, text?: string) {
-    return (`
-    <button class="px-3 pt-1 pb-2 bg-primary-300/30 border-primary-700 hover:bg-primary-300/70 shadow active:scale-[.97] border rounded transition-all" id="${id}" type="button">${text || ''}</button>
-    `);
-}
-
-function Para(text: string) {
-    return (`
-    <p class="">${text}</p>
-    `);
-}
 
 const app = document.querySelector<HTMLDivElement>('#app')!;
 app.innerHTML =
