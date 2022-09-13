4ec5113ddffe504741715af9c90b4403c36e1aef
Max Zakharzhevskiy <maxzz@msn.com>
2022-09-12 19:43:56 -0700

    WIP. @pmac/template. add access to sameDc with getSameDc()

--- packages/template/src/utils/report-data.ts
+++ packages/template/src/utils/report-data.ts
@@ -1,1 +1,16 @@
+import { ItemInputFile, Report } from '@pmac/shared-types';
+
 export { reports as reportData } from './test-data';
+
+function getInputs(report: Report): Record<string, ItemInputFile> {
+    const inputs = report.inputs?.input?.reduce<Record<string, ItemInputFile>>((acc, val) => {
+        acc[val.id] = val;
+        return acc;
+    }, {});
+    return inputs || {};
+}
+
+export function getSameDc(report: Report): ItemInputFile[] {
+    const inputs =getInputs(report);
+    return report.domcreds?.multiple?.map((item) => inputs[item.id]) || [];
+}
