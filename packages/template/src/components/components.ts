82d2f7ff297b00d39a0d59c802a749583a95a00c
Max Zakharzhevskiy <maxzz@msn.com>
2022-09-12 17:29:48 -0700

    WIP. @pmac/template. update components layout

--- packages/template/src/components/components.ts
+++ packages/template/src/components/components.ts
@@ -1,20 +1,15 @@
 export function H1({text}: {text: string}) {
-    return (`
-    <h1 class="text-lg font-semibold" title="List of all files">${text}</h1>
-    `);
-}
-
-export function Button({ id, text = '' }: { id: string, text?: string; }) {
-    return (`
-    <button class="px-3 pt-1 pb-2 
-        bg-primary-300/30 border-primary-700 hover:bg-primary-300/70 shadow active:scale-[.97] border rounded transition-all" 
-        id="${id}" type="button">${text}
-    </button>
-    `);
+    return `<h1 class="text-lg font-semibold" title="List of all files">${text}</h1>`;
 }
 
 export function Para({text}: {text: string}) {
-    return (`
-    <p class="">${text}</p>
-    `);
+    return `<p class="">${text}</p>`;
+}
+
+export function Button({ id, text = '' }: { id: string, text?: string; }) {
+    return `
+    <button class="px-3 pt-1 pb-2 
+        bg-primary-300/30 border-primary-700 hover:bg-primary-300/70 shadow active:scale-[.97] border rounded transition-all" 
+        id="${id}" type="button">${text}
+    </button>`;
 }
--- packages/template/src/main.ts
+++ packages/template/src/main.ts
@@ -7,22 +7,20 @@ import '../index.css';
 function singleReport(single: Report) {
     const isEmpty = !Object.keys(single).length; //TODO: !report.inputs?.input?.length && !report.domcreds?.multiple?.length;
     return `
-<div class="px-4 p-2 grid grid-cols-[1fr_minmax(100px,auto)_1fr]">
-    <div class="col-start-2">
-        ${TableAllInputs(single?.inputs)}
-        ${isEmpty ? Para({text: 'There are no manifest files using domain credentials. Nothing changed'}) : ''}
-    </div>
-</div>
-`;
+        <div class="px-4 p-2 grid grid-cols-[1fr_minmax(100px,auto)_1fr]">
+            <div class="col-start-2">
+                ${TableAllInputs(single?.inputs)}
+                ${isEmpty ? Para({text: 'There are no manifest files using domain credentials. Nothing changed'}) : ''}
+            </div>
+        </div>`;
 }
 
 function App() {
-    return (`
+    return `
         <div class="flex-1 bg-gradient-to-t from-primary-50 to-[#036eb3] text-primary-900">
             ${PageHeader()}
             ${reports.map((report) => singleReport(report)).join('')}
-        </div>
-    `);
+        </div>`;
 }
 
 const app = document.querySelector<HTMLDivElement>('#app')!;
