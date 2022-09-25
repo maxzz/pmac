3866f931fc4b8b0195b731fb88d6f8edd0c0714c
Max Zakharzhevskiy <maxzz@msn.com>
2022-09-25 00:29:04 -0700

    WIP. @pmac/template. update main scrollbar location

--- packages/template/src/main.ts
+++ packages/template/src/main.ts
@@ -9,12 +9,14 @@ function App() {
     return `
         <div class="h-full grid grid-rows-[auto_minmax(0,1fr)_auto] text-sky-800">
             ${Section1_Header()}
-            <div class="mx-auto h-full grid grid-rows-[auto_1fr] overflow-y-auto">
-                ${Section2_About()}
-                <div>
-                    <main id="report-table"></main>
-                    <div class="pb-4 px-4 max-w-[80ch] animate-slide-down hidden" id="general-info">
-                        ${Section4_GeneralInfo()}
+            <div class="w-full h-full overflow-auto">
+                <div class="mx-auto md:w-max h-full grid grid-rows-[auto_1fr]">
+                    ${Section2_About()}
+                    <div>
+                        <main id="report-table"></main>
+                        <div class="pb-4 px-4 max-w-[80ch] animate-slide-down hidden" id="general-info">
+                            ${Section4_GeneralInfo()}
+                        </div>
                     </div>
                 </div>
             </div>
