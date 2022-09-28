1f0b2bce66a390d7a5dfd218aa6de21b17232f22
Max Zakharzhevskiy <maxzz@msn.com>
2022-09-27 23:06:05 -0700

    WIP. @pmac/utility. remove json and restore test-data.ts

---
+++ packages/template/src/utils/test-data.ts
@@ -0,0 +1,12 @@
+import { ReportRecords } from '@pmac/shared-types';
+import jsonData from './test-data.json';
+
+if (process.env.NODE_ENV !== 'production') {
+
+    const testReports: ReportRecords = jsonData;
+    console.log('only dev report', testReports);
+
+    (window as any)['tmReport'] = testReports;
+}
+
+export const reports: ReportRecords = (window as any)['tmReport'];
