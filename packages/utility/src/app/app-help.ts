4c65e2aa37919368cfe8f218bbe5e4d08dec02b9
Max Zakharzhevskiy <maxzz@msn.com>
2022-10-22 18:23:10 -0700

    WIP. utility. update help

--- packages/utility/src/app/app-help.ts
+++ packages/utility/src/app/app-help.ts
@@ -5,19 +5,55 @@ let config = require('../../../../package.json');
 export const programName = config.name; //config.name.split('/')[0].substring(1);
 export const programVersion = config.version;
 
+export const strDoneNothing = 'Nothing has been done';
+
 export function help() {
 
     let msg = `
-The ${color.cyan(`${programName}`)} (password manager administrator commands) utility version ${programVersion}.
+The ${color.gray(`${programName}`)} (password manager administrator commands) utility version ${programVersion}.
 
-Usage: ${color.cyan(`${programName} source [options]`)}
+Usage: ${color.gray(`${programName} source [command] [options]`)}
 
 where:
-    source       - folder with the manifest files to process or
-                   space-separated list of manifest filenames to process
+    source              - folder with the manifest files to process or
+                          space-separated list of manifest filenames to process
+
+    command:
+        --dc, -c        - switch domain credentials to credentials that apply only
+                          to a specific URL
+        --add-prefix    - add domain name as prefix to manifest filenames
+        --remove-prefix - remove domain name prefix from manifest filenames
+
     options:
-        --domain - process manifests only with the specified web domain.
-                   For example pmac --domain google.com
+        --domain        - process manifests only with the specified web domain.
+
+examples
+    ${color.gray('pmac --add-prefix --domain mybank.com c:/manifests')}
+        This command will look in the "c:/manifests" folder and will rename
+        manifests with the "mybank.com" domain to "mybank.com.{guid}.dpm".
+
+    ${color.gray('pmac --remove-prefix --domain mybank.com c:/manifests')}
+        This command will look in the "c:/manifests" folder and will rename
+        manifests file names "mybank.com.{guid}.dpm" to "{guid}.dpm".
+`;
+    console.log(msg);
+}
+
+/*
+
+For example ${color.gray('pmac --domain google.com')}
+
+examples
+
+    ${color.gray('pmac --add-prefix --domain mybank.com c:/manifests')}
+
+        --add-prefix is commnad
+        --domain mybank.com is option
+        c:/manifests is source
+
+        This command will look in the "c:/manifests" folder and will rename
+        manifests with the "mybank.com" domain to "mybank.com.{guid}.dpm".
+
 
 The ${color.cyan(`${programName}`)} utility converts PM manifests with domain credentials to
 manifests with credentials that apply only to a specific URL.
@@ -26,11 +62,7 @@ ${color.gray(`Run this utility from the folder where the manifest files are
 located. Alternatively, you can specify the folder where the
 manifest files are located or filenames separated by space
 character. Specify the folder name or file names, but not both.`)}
-
-Version ${programVersion}`;
-
-    console.log(msg);
-}
+*/
 
 //TODO: options: replace files i.e. don't create backup folder (unique backup name w/ date)
 //TODO: options: interactive mode
@@ -39,17 +71,19 @@ export type MinimistArgs = {
     'dc': boolean;
     'add-prefix': boolean;
     'remove-prefix': boolean;
-    'd': boolean;
+    'c': boolean;
     'a': boolean;
     'r': boolean;
+    'domain': string;
     _: string[];
 };
 
 export function getMinimistArgs(): MinimistArgs {
     let args: MinimistArgs = minimist<MinimistArgs>(process.argv.slice(2), {
         boolean: ['dc', 'add-prefix', 'remove-prefix'],
+        string: ['domain'],
         alias: {
-            'd': 'dc',
+            'c': 'dc',
             'a': 'add-prefix',
             'r': 'remove-prefix',
         },
