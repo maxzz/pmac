812ad0ed25f489cbe9fb07e1eaebdae471a97a43
Max Zakharzhevskiy <maxzz@msn.com>
2022-10-28 16:58:43 -0700

    WIP. utility. add numberOfDomCreds instead of hasDomCredsReport() check

--- packages/utility/src/utils/utils-app-report-template.ts
+++ packages/utility/src/utils/utils-app-report-template.ts
@@ -3,6 +3,6 @@ import { TargetGroup } from "../app-types";
 const reportData = require('@pmac/template');
 export const templateStr = Buffer.from(reportData.template.skeleton, 'base64').toString();
 
-export function hasDomCredsReport(targetGroup: TargetGroup) {
-    return !!targetGroup.report.domcreds?.multiple?.length;
+export function numberOfDomCreds(targetGroup: TargetGroup): number {
+    return targetGroup.report.domcreds?.multiple?.length || 0;
 }
