94385dbcd53c8182f974912ac9a0558795eedb4b
Max Zakharzhevskiy <maxzz@msn.com>
2022-10-28 17:16:08 -0700

    WIP. utility. add prototype for rename operation

--- packages/utility/src/app/task-rename/task-rename-files.ts
+++ packages/utility/src/app/task-rename/task-rename-files.ts
@@ -3,16 +3,19 @@ import { appOptions, notes } from "../app-env";
 import { filterFilesByDomain } from "../../utils";
 import { step1_LoadManifests } from "../task-common";
 
-function processRootGroup(rootGroup: RootGroup): TargetGroup {
+function processRootGroup(rootGroup: RootGroup) {
     const targetGroup = step1_LoadManifests(rootGroup);
     filterFilesByDomain(targetGroup, appOptions.domain);
 
-    return targetGroup;
+    targetGroup.files.forEach((fileMeta) => {
+        console.log('    ', fileMeta.short);
+    });
+
 }
 
 export function executeTaskRename(rootGroups: RootGroup[], addOrRemove: boolean) {
-    throw new Error('Not implemented yet');
-    
-    const targetGroups = rootGroups.map(processRootGroup);
+    //throw new Error('Not implemented yet');
+
+    rootGroups.forEach(processRootGroup);
     notes.add(`All done`);
 }
