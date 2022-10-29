aed7f2757d943f97c12b42f71f36b5a7fa540954
Max Zakharzhevskiy <maxzz@msn.com>
2022-10-28 17:04:10 -0700

    WIP. utility. step-make-report.ts code clean up

--- packages/utility/src/app/task-dc/step-make-report.ts
+++ packages/utility/src/app/task-dc/step-make-report.ts
@@ -1,6 +1,6 @@
 import path from "path";
 import fs from "fs";
-import { ItemError, ReportFormUrls, Report, ReportRecords } from "@pmac/shared-types";
+import { Report } from "@pmac/shared-types";
 import { color, templateStr, toUnix } from "../../utils";
 import { TargetGroup } from "../../app-types";
 import { appOptions } from "../app-env";
@@ -33,44 +33,6 @@ export function step3_4_MakeTargetGroupReport(targetGroup: TargetGroup): void {
     }
 }
 
-export function step4_FinalMakeReportToAllGroups(targetGroups: TargetGroup[]): void {
-
+export function step4_MakeReportToAllGroups(targetGroups: TargetGroup[]): void {
     createJsonForDebugging(targetGroups);
-
-    /*
-    const report: ReportRecords = targetGroups.map((targetGroup) => ({ ...targetGroup.report, root: toUnix(targetGroup.root) }));
-    const dataStr = JSON.stringify(report, null, 4);
-    
-    console.log('dataStr:\n', dataStr);
-    
-    templateStr.replace('"__INJECTED__DATA__"', dataStr);
-    */
-
-
-
-    // function makeHtmlReport(targetGroup: TargetGroup): string | undefined {
-    //     if (Object.keys(targetGroup.report).length) {
-    //         const dataStr = JSON.stringify(targetGroup.report, null, 4);
-    //         console.log('dataStr:\n', dataStr);
-    //         return templateStr.replace('"__INJECTED__DATA__"', dataStr);
-    //     }
-    // }
-    //
-    // targetGroups.forEach((targetGroup) => {
-    //     const report = makeHtmlReport(targetGroup);
-    //
-    //     if (targetGroup.sameDc.length) {
-    //         printDcActive(targetGroup.sameDc);
-    //     } else {
-    //         notes.add(`\nNothing done:\nThere are no duplicates in ${targetGroup.files.length} loaded file${targetGroup.files.length === 1 ? '' : 's'}.`);
-    //     }
-    //
-    //     if (report) {
-    //         //TODO: save it into the same folder
-    //         //console.log('newTemplate\n', report);
-    //         console.log(color.gray(`newTemplate: ${report.substring(0, 100).replace(/\r?\n/g, ' ')}`));
-    //     }
-    //
-    //     notes.add(`All done in folder ${targetGroup.root}`);
-    // });
 }
