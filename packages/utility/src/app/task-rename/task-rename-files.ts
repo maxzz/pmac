ac1c116052d9766b7aee3fc68ab69618a35cbeae
Max Zakharzhevskiy <maxzz@msn.com>
2022-10-29 18:11:39 -0700

    WIP. utility. rename. add addOrRemove condition

--- packages/utility/src/app/task-rename/task-rename-files.ts
+++ packages/utility/src/app/task-rename/task-rename-files.ts
@@ -1,20 +1,36 @@
 import { RootGroup, TargetGroup } from "../../app-types";
 import { appOptions, notes } from "../app-env";
-import { filterFilesByDomain } from "../../utils";
+import { color, filterFilesByDomain } from "../../utils";
 import { step1_LoadManifests } from "../task-common";
+import path from "path";
 
-function processRootGroup(rootGroup: RootGroup) {
+function processRootGroup(rootGroup: RootGroup, addOrRemove: boolean) {
     const targetGroup = step1_LoadManifests(rootGroup);
     filterFilesByDomain(targetGroup, appOptions.domain);
 
     targetGroup.files.forEach((fileMeta) => {
-        const filenameWPerix = fileMeta.short.match(/(.*)({c857ed8e-5e08-48c2-ac5e-be1495b7b5a3})(.*)\.dpm/);
-        console.log('    ', fileMeta.short, filenameWPerix);
+        const dirname = path.dirname(fileMeta.short);
+        const filename = path.basename(fileMeta.short);
+        //console.log(`    dir: ${dirname} filename: ${filename}`);
+
+        const m = filename.match(/(.*)({[a-zA-Z0-9]{8,8}-[a-zA-Z0-9]{4,4}-[a-zA-Z0-9]{4,4}-[a-zA-Z0-9]{4,4}-[a-zA-Z0-9]{12,12}})(.*)\.dpm/);
+        if (m) {
+            const [, prefix, name, suffix] = m;
+            console.log(`prefix: '${prefix}' guid: ${name} suffix: '${suffix}'`); //TODO: 'C\\{63b8feef-c560-4777-b26a-70413303c096}.dpm', // path.basename
+
+            if (addOrRemove) {
+
+            } else {
+
+            }
+        } else {
+            console.log(color.red(`no match ${fileMeta.short}`));
+        }
     });
 }
 
 export function executeTaskRename(rootGroups: RootGroup[], addOrRemove: boolean) {
     //throw new Error('Not implemented yet');
-    rootGroups.forEach(processRootGroup);
+    rootGroups.forEach((rootGroup) => processRootGroup(rootGroup, addOrRemove));
     notes.add(`All done`);
 }
