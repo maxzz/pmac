04cfe6c95203828915b6a188ec80dbb2e38cd2f3
Max Zakharzhevskiy <maxzz@msn.com>
2022-10-30 14:44:22 -0700

    WIP. utility. rename. switch getAutoName() check to domain___ending shape

--- packages/utility/src/app/task-rename/task-rename-files.ts
+++ packages/utility/src/app/task-rename/task-rename-files.ts
@@ -4,17 +4,25 @@ import { color, filterFilesByDomain } from "../../utils";
 import { step1_LoadManifests } from "../task-common";
 import path from "path";
 
-function getAutoName(prefix: string): {prefix: string, isAuto: boolean} {
-    const mOur = prefix.match(/^([\s\S]*)(___)/); //TODO: place it anywhere
+function getAutoName(prefix: string): { prefix: string; ourAutoNmae: boolean; ending: string; } {
+    const mOur = prefix.match(/^([\s\S]*)___([\s\S]*)/); //TODO: place it anywhere
     return {
         prefix: mOur ? mOur[1] : prefix,
-        isAuto: !!mOur,
-    }
+        ending: mOur ? mOur[2] : '',
+        ourAutoNmae: !!mOur,
+    };
 }
+
+type RenamePair = { //TODO: check length root + dir + new name < 255
+    oldName: string;
+    newName: string;
+};
 
 function processRootGroup(rootGroup: RootGroup, addOrRemove: boolean) {
     const targetGroup = step1_LoadManifests(rootGroup);
     filterFilesByDomain(targetGroup, appOptions.domain);
+
+    const renamePairs = [];
 
     targetGroup.files.forEach((fileMeta) => {
         const dirname = path.dirname(fileMeta.short);
@@ -26,8 +34,12 @@ function processRootGroup(rootGroup: RootGroup, addOrRemove: boolean) {
             const [, prefixRaw, name, suffix] = m;
 
             const domain = fileMeta.urls?.[0].oParts?.domain || '';
+            if (!domain) {
+                console.log(color.yellow(`${filename} not a website`));
+                return;
+            }
 
-            const {prefix, isAuto} =  getAutoName(prefixRaw);
+            const { prefix, ourAutoNmae: isAuto } = getAutoName(prefixRaw);
 
             console.log(`url: ${domain}___${name}${suffix}.dpm was: ${isAuto ? '___' : '   '} auto:'${prefix}' a:'${prefixRaw}'`); //TODO: 'C\\{63b8feef-c560-4777-b26a-70413303c096}.dpm', // path.basename
             // console.log(`b: ${name} c:'${suffix}' ${isAuto ? '___' : '   '} auto:'${prefix}' url: ${domain} a:'${prefixRaw}'`); //TODO: 'C\\{63b8feef-c560-4777-b26a-70413303c096}.dpm', // path.basename
