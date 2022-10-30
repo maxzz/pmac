c2589fa55a1853f15370581dbad06b9db5d65d04
Max Zakharzhevskiy <maxzz@msn.com>
2022-10-30 16:07:17 -0700

    WIP. utility. move winapp highlight from renamePair[] data

--- packages/utility/src/app/task-rename/task-rename-files.ts
+++ packages/utility/src/app/task-rename/task-rename-files.ts
@@ -17,6 +17,8 @@ type RenamePair = { //TODO: check length root + dir + new name < 255
     newName: string;
 };
 
+const winappPrefix = 'winapp';
+
 function processRootGroup(rootGroup: RootGroup, addOrRemove: boolean) {
     const targetGroup = step1_LoadManifests(rootGroup);
     filterFilesByDomain(targetGroup, appOptions.domain);
@@ -26,18 +28,11 @@ function processRootGroup(rootGroup: RootGroup, addOrRemove: boolean) {
     targetGroup.files.forEach((fileMeta) => {
         const dirname = path.dirname(fileMeta.short);
         const filename = path.basename(fileMeta.short);
-        //console.log(`    dir: ${dirname} filename: ${filename}`);
 
         const m = filename.match(/(.*)({[a-zA-Z0-9]{8,8}-[a-zA-Z0-9]{4,4}-[a-zA-Z0-9]{4,4}-[a-zA-Z0-9]{4,4}-[a-zA-Z0-9]{12,12}})(.*)\.dpm/);
         if (m) {
             const [, prefixRaw, guid, suffix] = m;
-
-            const domain = fileMeta.urls?.[0].oParts?.domain || 'winapp';
-            if (!domain) {
-                console.log(color.yellow(`${filename} not a website`));
-                return;
-            }
-
+            const domain = fileMeta.urls?.[0].oParts?.domain || winappPrefix;
             const { ourAutoName, ending } = getAutoName(prefixRaw, domain);
 
             if (addOrRemove) {
@@ -54,7 +49,7 @@ function processRootGroup(rootGroup: RootGroup, addOrRemove: boolean) {
 
                     renamePairs.push({
                         oldName: path.join(rootGroup.root, fileMeta.short),
-                        newName: domain === 'winapp' ? color.yellow(fullName) : fullName,
+                        newName: fullName,
                     });
                 } else {
                     console.log(color.green(`${filename} already our name`));
@@ -62,17 +57,16 @@ function processRootGroup(rootGroup: RootGroup, addOrRemove: boolean) {
             } else {
 
             }
-
-            //console.log(`url: ${domain}___${guid}${suffix}.dpm was: ${ourAutoName ? '___' : '   '} a:'${ending}'`); //TODO: 'C\\{63b8feef-c560-4777-b26a-70413303c096}.dpm', // path.basename
-            // console.log(`b: ${guid} c:'${suffix}' ${isAuto ? '___' : '   '} auto:'${prefix}' url: ${domain} a:'${prefixRaw}'`); //TODO: 'C\\{63b8feef-c560-4777-b26a-70413303c096}.dpm', // path.basename
-
         } else {
-            console.log(color.red(`no match ${fileMeta.short}`));
+            notes.add(color.red(`${fileMeta.short} has no guid filename match`));
         }
     });
 
-    //console.log('renamePairs', renamePairs);
-    renamePairs.forEach((pair) => console.log(`{\n    ${pair.oldName}\n    ${pair.newName}\n}`));
+    renamePairs.forEach((pair) => {
+        const isWinApp = pair.newName.match(new RegExp(`${winappPrefix}___`));
+        const n = color[isWinApp ? 'yellow' : 'green'](pair.newName);
+        console.log(`{\n    ${pair.oldName}\n    ${n}\n}`);
+    });
 }
 
 export function executeTaskRename(rootGroups: RootGroup[], addOrRemove: boolean) {
