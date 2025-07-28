04faa1be8bbe67ea5ff35dbf128b60dffa2ff1b6
Max Zakharzhevskiy <maxzz@msn.com>
2025-07-27 19:58:03 -0700

    WIP. update 2025. utility. step3_1_MakeBackupCopy() code review. 2

--- packages/utility/src/app/2-task-domain-creds/3-step-3-save-result.ts
+++ packages/utility/src/app/2-task-domain-creds/3-step-3-save-result.ts
@@ -1,7 +1,7 @@
 import { type SingleFolder } from "../9-types";
 import { ensureNameUnique, nowDayTime } from "../../utils";
 import { appOptions } from "../8-app-env";
-import { step3_1_MakeBackupCopy } from "./4-step-3-1-make-changes";
+import { step3_1_MakeFilesBackupCopy } from "./4-step-3-1-make-changes";
 import { step3_2_Modify } from "./4-step-3-2-modify";
 import { step3_3_Save } from "./4-step-3-3-save";
 import { step3_4_MakeSingleFolderReport } from "./5-step-3-4-make-report-target";
@@ -17,7 +17,7 @@ export function step3_SaveResult(singleFolder: SingleFolder): void {
         singleFolder.backupFolder = ensureNameUnique(`${singleFolder.rootFolder}/backup-${nowDayTime().replace(/ /g, '-')}`, false);
 
         if (appOptions.needBackup) {
-            step3_1_MakeBackupCopy(singleFolder);
+            step3_1_MakeFilesBackupCopy(singleFolder);
         }
 
         if (appOptions.needUpdate) {
--- packages/utility/src/app/2-task-domain-creds/4-step-3-1-make-changes.ts
+++ packages/utility/src/app/2-task-domain-creds/4-step-3-1-make-changes.ts
@@ -4,10 +4,10 @@ import { OsStuff } from "../../utils";
 import { type FileCnt, type SingleFolder } from "../9-types";
 import { addError, getFileCntsFromDuplfileCnts } from "../4-common-tasks";
 
-export function step3_1_MakeBackupCopy(singleFolder: SingleFolder): void {
+export function step3_1_MakeFilesBackupCopy(singleFolder: SingleFolder): void {
     try {
         const fileCnts: FileCnt[] = getFileCntsFromDuplfileCnts(singleFolder.duplFileCnts);
-        makeBackupCopyFiles(fileCnts, singleFolder.backupFolder);
+        makeFilesBackupCopy(fileCnts, singleFolder.backupFolder);
     } catch (error) {
         addError(singleFolder, {
             text: `Nothing done:\nCannot create backup: the destination path is too long or there is not enough permissions.\nFolder:\n${singleFolder.rootFolder}`,
@@ -17,12 +17,13 @@ export function step3_1_MakeBackupCopy(singleFolder: SingleFolder): void {
     }
 }
 
-function makeBackupCopyFiles(fileCnts: FileCnt[], destFolder: string): void {
+function makeFilesBackupCopy(fileCnts: FileCnt[], destFolder: string): void {
     OsStuff.mkdirSync(destFolder);
     fileCnts.forEach(
         (fileCnt) => {
             const fname = path.join(destFolder, fileCnt.relativeFname);
             const maybeSubFolder = path.dirname(fname);
+
             OsStuff.mkdirSync(maybeSubFolder);
             fs.writeFileSync(fname, fileCnt.rawFileContent);
         }
